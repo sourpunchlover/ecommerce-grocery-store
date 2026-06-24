72187902b778d7c8b17b36296c4950dbcde18e8b
sourpunchlover <sndgpdesigns@gmail.com>
2026-06-24 15:08:19 -0400

    In ProfileService, added getbyUserId() & update(). Added code to update().

--- src/main/java/org/yearup/service/ProfileService.java
+++ src/main/java/org/yearup/service/ProfileService.java
@@ -1,6 +1,7 @@
 package org.yearup.service;
 
 import org.springframework.stereotype.Service;
+import org.yearup.models.Category;
 import org.yearup.models.Profile;
 import org.yearup.repository.ProfileRepository;
 
@@ -17,5 +18,26 @@ public class ProfileService
     public Profile create(Profile profile)
     {
         return profileRepository.save(profile);
+    }
+
+    public Profile getByUserId(int userId) {
+        return profileRepository.findById(userId).orElse(null);
+
+    }
+
+    public Profile update(int userId, Profile profile) {
+        Profile existing = profileRepository.findById(userId).orElseThrow();
+        existing.setFirstName(profile.getFirstName());
+        existing.setLastName(profile.getLastName());
+        existing.setPhone(profile.getPhone());
+        existing.setEmail(profile.getEmail());
+        existing.setAddress(profile.getAddress());
+        existing.setCity(profile.getCity());
+        existing.setState(profile.getState());
+        existing.setZip(profile.getZip());
+
+
+        return profileRepository.save(existing);
+
     }
 }
